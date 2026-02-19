5d8b85a516a5aecdb7e891122c1f648fdf5f4253
Dasha <dudko.dasha72773@gmail.com>
2026-02-19 12:15:42 +0000

    .

---
+++ task2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct Product {
+    char name[50];
+    double price;
+    int amount;   // інша назва змінної
+};
+
+int main() {
+    Product p;
+
+    cout << "Enter name: ";
+    cin >> p.name;
+
+    cout << "Enter price: ";
+    cin >> p.price;
+
+    cout << "Enter quantity: ";
+    cin >> p.amount;
+
+    cout << "Total: " << p.price * p.amount << " UAH";
+
+    return 0;
+}  
